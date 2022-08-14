cc1cc65145c2c1e6ffe4519a4a88999c9e252afc
Elshuye2112 <basie.elsh2112@gmail.com>
2022-08-14 09:52:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+// import HelloWorld from './components/HelloWorld.vue'
+const name = 'HelloWorld'
+let age=10
+let type='text'
+
+
+
+
+</script>
+
+<template>
+ <h1>Hello</h1>
+ <p>{{name}}{{age}}{{type}}</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
